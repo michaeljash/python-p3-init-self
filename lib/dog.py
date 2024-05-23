c8fc70693275d32a57a6cfe0057df5c35e1ac0c4
jashonmichael <jashon.michael@student.moringaschool.com>
2024-05-23 14:27:30 +0300

    complete lab

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
 
 class Dog:
-    pass
+    def __init__(self, name, favorite_toy="Any"):
+        self.name = name
+        self.favorite_toy = favorite_toy
+
+fido = Dog("Fido")
+fido.favorite_toy
+
+snoopy = Dog("Snoopy", "Tennis Ball")
+snoopy.favorite_toy
--- lib/person.py
+++ lib/person.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
 
 class Person:
-    pass
+    def __init__(self,name):
+        self.name = name
+
+Mike = ("Mike")
+Mike.name
+
+
